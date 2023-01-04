645ce49160d27c36cc8fa50366e85a246d663993
priteshrane <priteshrane01@gmail.com>
2023-01-04 21:46:08 +0530

    Level: Easy - Palindrome Number

---
+++ src/main/kotlin/easy/Palindrome.kt
@@ -0,0 +1,18 @@
+package easy
+
+fun main() {
+
+    println("Palindrome Number")
+
+    val x = 121
+
+    println("Input: $x")
+
+    println()
+    println("Output: ${isPalindrome(x)}")
+}
+
+fun isPalindrome(x: Int) : Boolean{
+    val xReverse = x.toString().reversed()
+    return x.toString() == xReverse
+}
