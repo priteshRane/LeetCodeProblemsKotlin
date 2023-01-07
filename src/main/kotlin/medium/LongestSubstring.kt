fdbaef405842f29d3ce22e1064f0f8dc8cfec3b9
priteshrane <priteshrane01@gmail.com>
2023-01-07 23:17:08 +0530

    Level: Medium - Longest Substring

---
+++ src/main/kotlin/medium/LongestSubstring.kt
@@ -0,0 +1,37 @@
+package medium
+
+fun main() {
+
+    println("Longest Substring")
+
+    val s = "abcabcbb"
+
+    println("Input: s=$s")
+    println("Output: ${findLongestSubstring(s)}")
+}
+
+fun findLongestSubstring(s: String): Int {
+    var left = 0
+    var right = 0
+    val seen = mutableSetOf<Char>()
+    var max = 0
+
+    while (right<s.length) {
+        val c = s[right]
+        if (seen.add(c)) {
+            max = Math.max(max,(right -left) + 1)
+            right++
+        } else {
+            // remove earlier char
+            while(s[left]!=c) {
+                seen.remove(s[left])
+                left++
+            }
+            // remove actual char
+            seen.remove(c)
+            left++
+        }
+    }
+
+    return max
+}
